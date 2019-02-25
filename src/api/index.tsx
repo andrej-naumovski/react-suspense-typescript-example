e74d23fce219a1af869636d938a525e101b46ece
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-02-25 13:32:43 +0100

    change: Formatting issues.

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -36,9 +36,9 @@ interface TopAlbumsResponse {
 
 export const fetchTopArtists = (): Promise<Array<Artist>> =>
   fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=2204dec2ead1a4a6c3ff22e0e1f0d08f&format=json')
-  .then(response => response.json())
-  .then(response => { console.log(response); return response; })
-  .then((response: ArtistsResponse) => response.topartists.artist);
+    .then(response => response.json())
+    .then(response => { console.log(response); return response; })
+    .then((response: ArtistsResponse) => response.topartists.artist);
 
 export const fetchArtistDetails = (name: string): Promise<Artist> =>
   fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${name}&api_key=2204dec2ead1a4a6c3ff22e0e1f0d08f&format=json`)
